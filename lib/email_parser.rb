43d05fb6a4a26f94ea4090bb6d81ad6be26d99c7
George Blackmore <gtblackmore@gmail.com>
2019-12-30 00:41:31 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,12 +2,17 @@ class EmailAddressParser
 
   attr_accessor :email
 
-  def initialize(email_addresses)
-    email_addresses.split(", ", " ")
-    
-      
-    
-  
-  
+  def initialize
+   @emails = emails
   end
+  
+  def parse
+    addresses = @emails.split(", ")
+    addresses = @emails.split(" ")
+    parsed_emails = []
+    addresses.each do |address|
+      parsed_emails << address.uniq
+    end
+  end
+  parsed_emails
 end
