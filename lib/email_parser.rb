1a0b03f8d5a62a95bb92ff59619b5f666c9c7ee1
George Blackmore <gtblackmore@gmail.com>
2019-12-30 01:01:50 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailAddressParser
     addresses = emails.split(/[\,\s]+/)
     collection = []
     addresses.select do |address|
-      collection << address
+      collection << address.uniq
     end
   collection
   end
